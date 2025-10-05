dfe8c2b9f44a8c2b1faddfd414fe6d64ce6c840d
tuk4ngKetik <tuk4ng.ketik@gmail.com>
2025-10-05 11:57:14 +0700

    lib/Helper/form_ipserver.dart
    BUG FIXED LIst users

--- lib/user_list.dart
+++ lib/user_list.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_typing_uninitialized_variables, use_function_type_syntax_for_parameters, unnecessary_null_comparison
 
 import 'package:astronacci_flutter/Api/api_user.dart'; 
-import 'package:astronacci_flutter/Controller/listuser_controller.dart';
-import 'package:astronacci_flutter/Controller/pencarian_controller.dart';
+import 'package:astronacci_flutter/Controller/listuser_controller.dart'; 
 import 'package:astronacci_flutter/Helper/form_pencarian.dart';
 import 'package:astronacci_flutter/Models/m_list_user.dart';
 import 'package:astronacci_flutter/user_detail.dart'; 
@@ -15,11 +14,8 @@ class UserList extends StatelessWidget {
   UserList({super.key, required this.header});
 
 
-  // final cariController = Get.put(PencarianController());
   final listController = Get.put(ListuserController());   
-  final apiUser = ApiUser();
-  // List<Datauser>? data = [];
-  // int? nextPage ;
+  final apiUser = ApiUser(); 
   
   
   @override 
@@ -29,16 +25,12 @@ class UserList extends StatelessWidget {
           FormPencarian(),  
           Expanded( child: 
            GetBuilder<ListuserController>(  
-              initState: (state) =>  listController.getListuser(),
+              initState: (state) => Future.microtask(() =>   listController.getListuser(),) ,
               builder: (controller) { 
             
                 if( controller.isLoading.value == true ){
                   return Center(child: CircularProgressIndicator(),);
-                }
-             
-                // if( cariController.isLoadingPencarian.value == true ){
-                //   return Center(child: CircularProgressIndicator(),);
-                // }
+                } 
              
                 
                 final data = controller.listDatauser;
